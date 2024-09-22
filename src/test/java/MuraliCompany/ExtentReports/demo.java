5ef9ac9c40b7081543d09a0b8490dc0943cc6076
unknown <muralikrishnaneela5@gmail.com>
2024-09-22 23:41:01 +0530

    fnal commit

---
+++ src/test/java/MuraliCompany/ExtentReports/demo.java
@@ -0,0 +1,11 @@
+package MuraliCompany.ExtentReports;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void demomethod() {
+		System.out.println("new demo method");
+	}
+}
